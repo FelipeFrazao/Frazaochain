2d65c4512ba4d4d0326734af928c958b0d5bc728
Felipe Frazão <felipe.sfrazao@outlook.com>
2018-10-08 20:53:13 -0300

    Register node included

--- api/frazaochain.py
+++ api/frazaochain.py
@@ -1,12 +1,14 @@
 import hashlib
 import json
 from time import time
+from urllib.parse import urlparse
 
 
 class Frazaochain(object):
     def __init__(self):
         self.current_transactions = []
         self.chain = []
+        self.nodes = set()
 
         # Create the genesis block
         self.new_block(previous_hash=1, proof=100)
@@ -91,3 +93,12 @@ class Frazaochain(object):
         guess = f'{last_proof}{proof}'.encode()
         guess_hash = hashlib.sha256(guess).hexdigest()
         return guess_hash[:4] == "0000"
+
+    def register_node(self, address):
+        """
+        Add a new node to the list of nodes
+        :param address: <str> Address of node. Eg. 'http://192.168.0.5:5000'
+        :return: None
+        """
+        parsed_url = urlparse(address)
+        self.nodes.add(parsed_url.netloc)
